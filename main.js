ad9d4bc95ba3117094986373ccfd4cfe5563958a
HekiNav <foobagames@gmail.com>
2025-05-19 09:17:45 +0300

    snapping

--- main.js
+++ main.js
@@ -8,10 +8,77 @@ const bigNumber = 100000
 const gridGap = 100;
 const initialScale = 0.5;
 const initialTranslate = [bigNumber / 2, bigNumber / 2];
+
 let svg
 let mapData = { lines: [] }
 let map
+let snapLineContainer
 let mode = null
+
+let lastNode = null
+let selectedLine = null
+
+const snappers = [
+    /* ADDING A NEW SNAPPER
+    // Copy from here =>
+    {
+        id: "string", //some descriptive id
+        function: (pos, prev, path) => { //pos: mouse position, prev: last placed node, path: selected line
+
+            //ADD CHECKS HERE
+
+            // if matches, return array: 
+            //  [
+            //      {
+            //          color: "CSScolor", 
+            //          line: [
+            //              {x: num, y: num}, //any amount of positions
+            //          ]
+            //      }, // any amount of matches
+            //  ]
+            // if no matches, return null
+        }
+    },
+    // <= To here
+     */
+    {
+        id: "otherNodes",
+        function: (pos, prev, path) => {
+            if (!pos || !path) return null
+            const color = "red"
+            let paths = []
+            path.points.forEach(n => {
+                // horizontal
+                if (pos.y == n.y ||
+                    // vertical
+                    pos.x == n.x ||
+                    // diagonal 1
+                    pos.x - pos.y == n.x - n.y ||
+                    // diagonal 2
+                    pos.x + pos.y == n.x + n.y
+                ) paths.push({ color: color, line: [{ x: pos.x, y: pos.y }, { x: n.x, y: n.y }] })
+            })
+            return paths.length ? paths : null
+        }
+    },
+    {
+        id: "previousNode",
+        function: (pos, prev, path) => {
+            if (!pos || !prev) return null
+            const color = "blue"
+            // horizontal
+            if (pos.y == prev.y ||
+                // vertical
+                pos.x == prev.x ||
+                // diagonal 1
+                pos.x - pos.y == prev.x - prev.y ||
+                // diagonal 2
+                pos.x + pos.y == prev.x + prev.y
+            ) return [{ color: color, line: [{ x: pos.x, y: pos.y }, { x: prev.x, y: prev.y }] }]
+            else return null
+        }
+    }
+]
 
 
 const lineHelper = d3.line().x(d => d.x).y(d => d.y).curve(circleCorners.radius(500))
@@ -29,6 +96,7 @@ function start() {
         .attr("height", height)
     const g = svg.append("g")
     map = g.append("g").attr("id", "map")
+    snapLineContainer = g.append("g").attr("id", "snapLines")
 
 
     //  ZOOM/PAN SYSTEM  //////////////////////////////////////////
@@ -80,12 +148,16 @@ function start() {
     svg.on("click", (e) => {
         switch (mode) {
             case "line":
-                if (!mapData.lines[0]) mapData.lines.push({
-                    id: 1,
-                    points: []
-                })
-                mapData.lines[0].points.push(snappedMouse(e, g))
-                console.log(mapData)
+                if (!mapData.lines[0]) {
+                    mapData.lines.push({
+                        id: 1,
+                        points: []
+                    })
+                    selectedLine = 1
+                }
+                const pos = snappedMouse(e, g)
+                mapData.lines[0].points.push(pos)
+                lastNode = pos
                 renderMap()
                 break;
             default:
@@ -109,11 +181,13 @@ function renderMap() {
         existingLineIds.push(l.id)
     })
     const newLines = mapData.lines
+    //adding lines
     newLines.forEach(l => {
         console.log(existingLineIds.some(id => id == l.id), existingLineIds)
-        if (existingLineIds.some(id => id == l.id)) return
+        if (existingLineIds.some(id => id == l.id)) return 
         addLine(l)
     })
+    // updating and deleting lines
     map.selectAll("path").each((d, i, n) => {
         if (newLines.some(l => l.id == n[i].id)) {
             //line is still in data, keep
@@ -134,6 +208,21 @@ function addLine(l) {
         .style('fill', 'none')
         .style('stroke', 'black')
         .style('stroke-width', '100')
+}
+function addSnapLine(snapLines) {
+    snapLines.forEach(snapLine => {
+        console.log(snapLine)
+        snapLineContainer
+            .append('path')
+            .datum(snapLine.line)
+            .attr('d', lineHelper)
+            .style('fill', 'none')
+            .style('stroke', snapLine.color)
+            .style('stroke-width', '20')
+    })
+}
+function clearSnapLines() {
+    snapLineContainer.selectChildren("*").each((d, i, n) => n[i].remove())
 }
 
 function zoomed(event) {
@@ -178,6 +267,12 @@ function snappedMouse(event, g) {
 }
 function snapMove(event, obj, g) {
     const mouse = snappedMouse(event, g)
+    clearSnapLines()
+    snappers.forEach(snap => {
+        const result = snap.function(mouse, lastNode, mapData.lines.find(l => l.id == selectedLine))
+        console.log(result)
+        if (result) addSnapLine(result)
+    })
     obj.attr("cx", mouse.x).attr("cy", mouse.y);
 }
 
