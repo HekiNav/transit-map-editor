7c51c23df1bac1974014cb8f3dd4ce091e7a68e7
Unto <developer.hekinav@gmail.com>
2026-03-31 16:25:02 +0300

    modes

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
                 Transit map editor
             </h1>
         </div>
-        <div class="grow w-full h-full overflow-hidden" id="map">
+        <div class="grow w-full h-full overflow-hidden relative" id="map">
+            <div class="absolute top-4 left-4 flex flex-col gap-2">
+                <div class="mode-button w-12 h-12 flex items-center justify-center bg-white rounded border-2 cursor-pointer" data-mode="move">Move</div>
+                <div class="mode-button w-12 h-12 flex items-center justify-center bg-white rounded border-2 cursor-pointer" data-mode="draw">Draw</div>
+            </div>
         </div>
     </div>
     `
@@ -47,14 +51,33 @@ window.addEventListener("resize", () => {
 
 
 new BasicCircleShape({ r: 20, x: 100, y: 0 })
-new StopShape({ x: 200, y: 0, w: 10, h: 50})
-
-console.log(getSelection())
-
+new StopShape({ x: 200, y: 0, w: 10, h: 50 })
 new Grid()
+
+document.querySelectorAll(".mode-button").forEach((el) => {
+    el.addEventListener("click", () => setMode(el.getAttribute("data-mode") || "move"))
+})
+
+svg.on("mode", () => {
+    const mode = svg.data("mode")
+    clearSelection()
+    svg.attr("style", mode == "draw" ? "cursor: crosshair" : "")
+    console.log("ujjj")
+})
+
+export function setMode(m: string) {
+    svg.data("mode", m)
+    svg.fire("mode")
+}
 
 svg.on("mouseup", (e) => {
     if (!(e as MouseEvent).shiftKey && !svg.data("moving")) {
         clearSelection()
     }
+})
+window.addEventListener("keyup", (e) => {
+    console.log((e as KeyboardEvent).code)
+    if ((e as KeyboardEvent).code == "Escape") setMode("move")
+    if ((e as KeyboardEvent).code == "KeyM") setMode("move")
+    if ((e as KeyboardEvent).code == "KeyD") setMode("draw")
 })
